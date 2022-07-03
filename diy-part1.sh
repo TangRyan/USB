91959ef0e9ff71c229787532116060df176fcb55
TangRyan <80312001+TangRyan@users.noreply.github.com>
2022-07-04 07:58:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,3 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i ' $a src-git liuran001_packages https://github.com/liuran001/openwrt-packages ' feeds.conf.default
 #sed -i ' $a src-git small8 https://github.com/kenzok8/small-package ' feeds.conf.default
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
